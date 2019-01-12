272627f963432a32f07dceb39ed5a178c0899000
shinying <shinying.lees@gmail.com>
2019-01-13 01:00:55 +0800

    Modify map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { MouseEvent } from '@agm/core';
 
+// just an interface for type safety.
+interface marker {
+	lat: number;
+	lng: number;
+	label?: string;
+	draggable: boolean;
+}
+
 @Component({
   selector: 'tovo-map',
   templateUrl: './map.component.html',
@@ -18,41 +26,7 @@ export class MapComponent implements OnInit {
   // google maps zoom level
   zoom: number = 15;
 
-  clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}`)
-  }
-  
-  mapClicked($event: MouseEvent) {
-    this.markers.push({
-      lat: $event.coords.lat,
-      lng: $event.coords.lng,
-      draggable: true
-    });
-  }
-  
-  markerDragEnd(m: marker, $event: MouseEvent) {
-    console.log('dragEnd', m, $event);
-  }
-  
   markers: marker[] = [
-	  {
-		  lat: 51.673858,
-		  lng: 7.815982,
-		  label: 'A',
-		  draggable: true
-	  },
-	  {
-		  lat: 51.373858,
-		  lng: 7.215982,
-		  label: 'B',
-		  draggable: false
-	  },
-	  {
-		  lat: 51.723858,
-		  lng: 7.895982,
-		  label: 'C',
-		  draggable: true
-	  }
   ]
 
   // slide toggle
@@ -70,10 +44,4 @@ export class MapComponent implements OnInit {
 
 }
 
-// just an interface for type safety.
-interface marker {
-	lat: number;
-	lng: number;
-	label?: string;
-	draggable: boolean;
-}
+
